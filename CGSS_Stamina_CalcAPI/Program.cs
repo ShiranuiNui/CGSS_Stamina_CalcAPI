850c040c0361d08cd65dc029f32d2823ca7e1450
ShiranuiNui <b1017120@fun.ac.jp>
2017-09-13 23:40:45 +0900

    プロジェクト ファイルを追加します。

---
+++ CGSS_Stamina_CalcAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CGSS_Stamina_CalcAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
