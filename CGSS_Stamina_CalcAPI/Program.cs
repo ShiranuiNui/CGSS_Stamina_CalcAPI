25793a177ac862a10e36315ade597191e657e0e8
ShiranuiNui <b1017120@fun.ac.jp>
2017-09-14 01:23:40 +0900

    [Add] Calculation of Stamina to Time Procedure

--- CGSS_Stamina_CalcAPI/Program.cs
+++ CGSS_Stamina_CalcAPI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Nancy.Hosting.Self;
 using Nancy;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CGSS_Stamina_CalcAPI
 {
@@ -26,6 +28,42 @@ namespace CGSS_Stamina_CalcAPI
         public HelloModule()
         {
             Get("/", args => "RUNNING!!!");
+        }
+    }
+    public class StaminaTime
+    {
+        public int CurrentStamina { get; private set; }
+        public int MaxStamina { get; private set; }
+        public TimeSpan CurrentRemainingTime { get; private set; }
+
+        public DateTime MaxStaminaTime { get; private set; }
+        public long MaxStaminaTime_Unix { get; private set; }
+        public string MaxStaminaTime_Unix_Str { get; private set; }
+        public StaminaTime(int current, int max, string remainingnext)
+        {
+            this.CurrentStamina = current;
+            this.MaxStamina = max;
+
+            string[] remaining_mmssArray = remainingnext.Split(":");
+            if (remaining_mmssArray.Length < 2)
+            {
+                throw new ArgumentException("CurrentRemainingTime Parse Error");
+            }
+
+            if (!int.TryParse(remaining_mmssArray[0], out int mm) || !int.TryParse(remaining_mmssArray[1], out int ss))
+            {
+                throw new ArgumentException("CurrentRemainingTime Parse Error");
+            }
+            if (mm > 5 || ss > 60)
+            {
+                throw new ArgumentOutOfRangeException("CurrentRemainingTime OutofRange Error");
+            }
+            this.CurrentRemainingTime = new TimeSpan(0, mm, ss);
+
+            var currentTime = DateTime.Now;
+            var expendedStamina = this.MaxStamina - this.CurrentStamina - 1;
+            var recoveryTimeSpan = new TimeSpan(0, expendedStamina * 5, 0);
+            this.MaxStaminaTime = currentTime.Add(CurrentRemainingTime).Add(recoveryTimeSpan);
         }
     }
 }
