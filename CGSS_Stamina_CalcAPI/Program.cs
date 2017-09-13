2527b04c38b65fa8b2d1da000ae6645856c0e012
ShiranuiNui <b1017120@fun.ac.jp>
2017-09-14 00:13:06 +0900

    [Add] Ready for SelfHosting

--- CGSS_Stamina_CalcAPI/Program.cs
+++ CGSS_Stamina_CalcAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nancy.Hosting.Self;
+using Nancy;
+using System;
 
 namespace CGSS_Stamina_CalcAPI
 {
@@ -7,6 +9,23 @@ namespace CGSS_Stamina_CalcAPI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            HostConfiguration hostConfigs = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+            using (var host = new NancyHost(new Uri("http://localhost:50120"), new DefaultNancyBootstrapper(), hostConfigs))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://*:50120");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get("/", args => "RUNNING!!!");
         }
     }
 }
